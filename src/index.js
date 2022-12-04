59d3273a2df22c511d041e5ea16f4a20ea241201
Beletskyi-Moawad Martin <m.beleckii@alef.im>
2022-12-04 15:46:38 +0300

    Не спи

--- src/index.js
+++ src/index.js
@@ -53,6 +53,11 @@ const voiceSender = (message, music_file) => {
 }
 
 client.on('message', async (message) => {
+  if(message.content === '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08') {
+    message.reply('Я тут');
+    return;
+  }
+
   if (message.author.bot) return;
 
   if (Object.keys(voice_messages).includes(message.content)) {
